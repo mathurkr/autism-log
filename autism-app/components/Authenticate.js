6fce3c08f9e177e60a7b607f024a30dd09923fa2
Andrew Chien <drewcch@gmail.com>
2019-12-02 21:14:54 -0800

    Fixed warning where UIManager via UIManager['getConstants'] was no longer supported (had to downgrade react-native-gesture-handler

--- autism-app/components/Authenticate.js
+++ autism-app/components/Authenticate.js
@@ -19,25 +19,27 @@ export default class Authenticate extends Component {
                 androidClientId: "751957114156-lvkj70nh3oaihov2dl82nufuq0ra7a1m.apps.googleusercontent.com",
                 iosClientId: "751957114156-gplohjh7bdca8l36rgtonsrchsr2sqrv.apps.googleusercontent.com",
                 scopes: ["profile", "email"]
-            })
+            });
 
             if (result.type === "success") {
                 this.setState({
                     loggedIn: true,
                     name: result.user.name,
                     // photoUrl: result.user.photoUrl
-                })
+                });
+                // this.props.navigation.navigate('Profile', { name: this.state.name });
             }
             else {
-                console.log("cancelled")
+                console.log("cancelled");
             }
         }
         catch (e) {
-            console.log("error", e)
+            console.log("error", e);
         }
     }
 
     render() {
+        const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
 
