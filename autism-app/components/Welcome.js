b96a1d33a0853443f4b273a6e85c9e9453dff3cf
joalmara <joalmara@uci.edu>
2020-01-18 17:34:02 -0800

    Carosuel on Welcome Page

--- autism-app/components/Welcome.js
+++ autism-app/components/Welcome.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Dimensions} from 'react-native';
 
 import { Button } from 'galio-framework';
 
 
 export default class Welcome extends Component {
     static navigationOptions = {
-        title: 'Welcome'
+        header: null
     };
 
     // state = {
@@ -26,43 +26,98 @@ export default class Welcome extends Component {
     }
 
     render() {
+        let screenWidth = Dimensions.get('window').width;
+        let screenHeight = Dimensions.get('window').height;
+        
         return (
-            <View style={styles.wrapper}>
-            <View style={styles.container}>
-                <Text style={styles.header}> Welcome to Luminous </Text>
-                <Text> Offering a better way to manage developmental disabilities and help children and adults with autism </Text>
-                <Button shadowless round color="#29d2e4" onPress={() => this._toSignUpPage()} style={styles.signup}> SIGN UP</Button>
-                <Button shadowless round color="#ffffff" onPress={() => this._toLoginPage()} style={styles.loginButton}>
-                    <Text>LOG IN</Text>
-                </Button>
-            </View>
-            </View>
+            <View style={styles.wrapper}> 
+
+                <ScrollView
+                horizontal ={true}
+                pagingEnabled= {true}
+                >
+
+                    <View style={{flex: 1,
+                    width: screenWidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'}}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.header}> Welcome to Luminous </Text>
+                            <Text style={styles.subTitle}> Offering a better way to manage developmental disabilities and help children and adults with autism </Text>
+                        </View>
+                    </View>
+
+
+                    <View style={{flex: 1,
+                    width: screenWidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'}}>
+                            <Text style={styles.header}> Capture events while they happen </Text>
+                            <Text style={styles.subTitle}>  Observe and monitor behaviors, actions, and day to day interactions, to better manage metltdown or burnouts </Text>
+                    </View>
+
+                    <View style={{flex: 1,
+                    width: screenWidth,
+                    justifyContent: 'center',
+                    alignItems: 'center'}}>
+                            <Text style={styles.header}> Welcome to Luminous </Text>
+                            <Text style={styles.subTitle}> Offering a better way to manage developmental disabilities and help children and adults with autism </Text>
+                    </View>
+
+                </ScrollView>
+
+                <View style={styles.buttonContainer}> 
+                    <Button shadowless round color="#29d2e4" onPress={() => this._toSignUpPage()} style={styles.signup}> SIGN UP</Button>
+                    <Button shadowless round color="#ffffff" onPress={() => this._toLoginPage()} style={styles.loginButton}>
+                        <Text>LOG IN</Text>
+                    </Button>
+                </View>
+
+        </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+
     wrapper: {
-        flex: 1
-    },
-    container: {
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
-        paddingLeft: 40,
-        paddingRight: 40
     },
+
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    buttonContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 50
+    },
+
+    textContainer:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 50
+    },
+
     header: {
         fontSize: 20,
         marginBottom: 27,
         fontWeight: 'bold',
-        marginTop: 36,
+        marginTop: 24,
         },
     
     subTitle: {
         fontSize: 16,
         fontWeight: 'normal',
-        lineHeight: 20
+        lineHeight: 20,
+        padding: 50
     },
 
     signup: {
@@ -77,4 +132,3 @@ const styles = StyleSheet.create({
     },
 
 });
-
