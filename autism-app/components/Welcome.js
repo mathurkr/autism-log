e964ff120cf3d6bab7b281b66cd25fac9b674eaf
joalmara <joalmara@uci.edu>
2020-01-19 14:03:25 -0800

    better alignment

--- autism-app/components/Welcome.js
+++ autism-app/components/Welcome.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, ScrollView, Dimensions} from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Dimensions, Image} from 'react-native';
 
 import { Button } from 'galio-framework';
 
@@ -26,109 +26,116 @@ export default class Welcome extends Component {
     }
 
     render() {
-        let screenWidth = Dimensions.get('window').width;
-        let screenHeight = Dimensions.get('window').height;
-        
         return (
-            <View style={styles.wrapper}> 
 
-                <ScrollView
-                horizontal ={true}
-                pagingEnabled= {true}
-                >
 
-                    <View style={{flex: 1,
-                    width: screenWidth,
-                    justifyContent: 'center',
-                    alignItems: 'center'}}>
-                        <View style={styles.textContainer}>
-                            <Text style={styles.header}> Welcome to Luminous </Text>
-                            <Text style={styles.subTitle}> Offering a better way to manage developmental disabilities and help children and adults with autism </Text>
-                        </View>
+            <View style={styles.container}>
+                <ScrollView horizontal={true} pagingEnabled={true} 
+                showsHorizontalScrollIndicator = {false} > 
+
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image}
+                        source={require('../assets/images/father_image.png')}/>
+                        <Text style={styles.title} > Welcome to Lumious </Text>
+                        <Text style={styles.description} > Offering a better way to manage developmental disabilities and help children and adults with autism </Text>
+                    </View>
+
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image}
+                        source={require('../assets/images/back_to_school.png')}/>
+                        <Text style={styles.title} > Capture events while they happen </Text>
+                        <Text style={styles.description} > Observe and monitor behaviors, actions, and day to day interactions, to better manage metldown or burnouts </Text>
                     </View>
 
 
-                    <View style={{flex: 1,
-                    width: screenWidth,
-                    justifyContent: 'center',
-                    alignItems: 'center'}}>
-                            <Text style={styles.header}> Capture events while they happen </Text>
-                            <Text style={styles.subTitle}>  Observe and monitor behaviors, actions, and day to day interactions, to better manage metltdown or burnouts </Text>
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image}
+                        source={require('../assets/images/undraw_data.png')}/>
+                        <Text style={styles.title} > Gather and use data for knowledge & insight</Text>
+                        <Text style={styles.description} > Aggregate and use customized visualizations to improve care and well being of those with autism </Text>
                     </View>
 
-                    <View style={{flex: 1,
-                    width: screenWidth,
-                    justifyContent: 'center',
-                    alignItems: 'center'}}>
-                            <Text style={styles.header}> Welcome to Luminous </Text>
-                            <Text style={styles.subTitle}> Offering a better way to manage developmental disabilities and help children and adults with autism </Text>
+
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image}
+                        source={require('../assets/images/undraw_community.png')}/>
+                        <Text style={styles.title} > Aggregate and share with your community </Text>
+                        <Text style={styles.description} > Use as a digital companion to help lead on the road to better health, productivity and happier lives. </Text>
                     </View>
+
+
+
 
                 </ScrollView>
 
-                <View style={styles.buttonContainer}> 
-                    <Button shadowless round color="#29d2e4" onPress={() => this._toSignUpPage()} style={styles.signup}> SIGN UP</Button>
-                    <Button shadowless round color="#ffffff" onPress={() => this._toLoginPage()} style={styles.loginButton}>
+                
+
+                <View style={styles.buttonContainer}>
+                    <Button shadowless round color="#29d2e4" onPress={() => this._toSignUpPage()} style={styles.signupBtn}> SIGN UP</Button>
+                    <Button shadowless round color="#ffffff" onPress={() => this._toLoginPage()} style={styles.loginBtn}>
                         <Text>LOG IN</Text>
                     </Button>
                 </View>
-
-        </View>
+                
+            </View>
+            
         );
     }
 }
 
 const styles = StyleSheet.create({
-
-    wrapper: {
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-
     container: {
         flex: 1,
-        alignItems: 'center',
+        //backgroundColor: '#3498db'
+    },
+    imageContainer : {
+        alignItems:'center',
+        flexGrow: 1,
         justifyContent: 'center',
+        marginTop:20,
+        width: Dimensions.get('window').width
+    }, 
+
+    image: {
+        width: 300,
+        height:300
+    },
+
+    title: {
+        marginTop: 10,
+        width: 297,
+        textAlign: 'center',
+        fontSize: 20,
+        color: '#414141',
+        fontWeight: 'bold'
+    },
+
+    description: {
+        marginTop: 10,
+        width: 297,
+        textAlign: 'center',
+        fontSize: 16,
+        color: '#737373'
     },
 
     buttonContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 50
+        padding: 20
     },
 
-    textContainer:{
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 50
+    signupBtn: {
+        height: 40,
+        marginBottom: 20,
+        paddingHorizontal: 10
     },
 
-    header: {
-        fontSize: 20,
-        marginBottom: 27,
-        fontWeight: 'bold',
-        marginTop: 24,
-        },
-    
-    subTitle: {
-        fontSize: 16,
-        fontWeight: 'normal',
-        lineHeight: 20,
-        padding: 50
-    },
-
-    signup: {
-       marginBottom: 11, 
-       marginTop: 103
-    },
-
-    loginButton: {
+    loginBtn: {
         borderWidth: 1,
         borderColor: '#000000',
         color: '#ffffff',
-    },
+        height: 40,
+        marginBottom: 40,
+        paddingHorizontal: 10
+    }
+
 
 });
