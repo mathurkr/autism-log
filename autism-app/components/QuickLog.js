6cee57d2e4355e4cb2385f6a91480c289bfcdffe
Kavi Mathur <mathurkr@uci.edu>
2020-01-20 16:41:46 -0800

    Starting Quick Log w/ basic parts

--- autism-app/components/QuickLog.js
+++ autism-app/components/QuickLog.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { TextInput } from 'react-native';
 
 import { Button } from 'galio-framework';
-
+// import Slider from '@react-native-community/slider';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default class QuickLog extends Component {
     static navigationOptions = {
         title: 'Quick Log'
     };
 
-    // state = {
-    //     signedUp: false,
-    //     loggedIn: false
-    //     // name: ''
-    // };
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+            date: new Date(),
+            
+        };
 
-  
+      }
+    
+    setDate = (event, date) => {
+        date = date || this.state.date;
+
+        this.setState({
+            show: Platform.OS === 'ios' ? true : false,
+            date,
+        });
+    }
+    
     render() {
         return (
             <View style={styles.container}>
                 <Text>Location</Text>
+                {/* Start with single-line text field, transition to location search if a library exists for react native. */}
+                <TextInput
+                    onChangeText={text => this.setState({text})}
+                    value={this.state.text}
+                />
                 <Text>Date</Text>
+                {/* Find date picker */}
+                <DateTimePicker value={date}
+                    mode='date'
+                    is24Hour={true}
+                    display="default"
+                    onChange={this.setDate} />
+                <Text>Meltdown Triggers</Text>
+                {/* Need 5 custom buttons with images inside of them. Maybe create a component? */}
+                <Text>Severity</Text>
+                {/* <Slider
+                    style={{width: 200, height: 40}}
+                    minimumValue={0}
+                    maximumValue={6}
+                    minimumTrackTintColor="#FFFFFF"
+                    maximumTrackTintColor="#000000"
+                /> */}
                 <Button shadowless round color="#ffffff" /* onPress={() => this._toLoginPage()} */ style={styles.submitButton}>
-                    <Text>Submit</Text>
+                    <Text>Submit </Text>
                 </Button>
             </View>
         );
@@ -30,6 +65,7 @@ export default class QuickLog extends Component {
 }
 
 const styles = StyleSheet.create({
+    
     container: {
         flex: 1,
         alignItems: 'center',
