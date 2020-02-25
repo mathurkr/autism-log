c8ebdd98c93ea357d97ee65c9bbdc0f9c70c982a
joalmara <joalmara@uci.edu>
2020-02-25 00:06:40 -0800

    Home Page Styles

--- autism-app/src/screens/IndexScreen.js
+++ autism-app/src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, Text, FlatList, Button,TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+import { View, Text, FlatList, Button,TouchableOpacity, StyleSheet, Dimensions, Image} from 'react-native';
 import {Context} from '../context/BlogContext';
 import { Ionicons } from '@expo/vector-icons';
 import {Feather} from '@expo/vector-icons'
@@ -9,34 +9,34 @@ import moment from "moment";
 let deviceWidth = Dimensions.get('window').width;
 
 
-renderTags = (item) =>{
-  return item.tags.map((tag, key) => {
-    return (
-      <TouchableOpacity key={key} style={styles.btnColor} onPress={() => {}}>
-          <View style={{flexDirection:"row"}}>
-              <Ionicons color="#0047cc" name={tag.icon} size={15}/>
-              <Text style={styles.tag}> {tag.name}</Text>
-           </View>
-      </TouchableOpacity> 
-    );
-  })
-}
+// renderTags = (item) =>{
+//   return item.tags.map((tag, key) => {
+//     return (
+//       <TouchableOpacity key={key} style={styles.btnColor} onPress={() => {}}>
+//           <View style={{flexDirection:"row"}}>
+//               <Ionicons color="#0047cc" name={tag.icon} size={15}/>
+//               <Text style={styles.tag}> {tag.name}</Text>
+//            </View>
+//       </TouchableOpacity> 
+//     );
+//   })
+// }
 
 
-renderTagIcon = (item) =>{
-  return item.tagsIcon.map((tag, key) => {
-    return (
-      <Ionicons name={tag} size={20} />
-    );
-  })
-}
+// renderTagIcon = (item) =>{
+//   return item.tagsIcon.map((tag, key) => {
+//     return (
+//       <Ionicons name={tag} size={20} />
+//     );
+//   })
+// }
 
 
 const IndexScreen = ({navigation}) => {
   const {state, deleteBlogPost} = useContext(Context);
 
   return (
-    <View style={{backgroundColor:'#EFEFEF'}}>                
+    <View style={{flex:1, backgroundColor:'#EFEFEF'}}>                
     <View>
 
     <CalendarStrip
@@ -44,7 +44,7 @@ const IndexScreen = ({navigation}) => {
             calendarAnimation={{ type: 'sequence', duration: 30 }}
             selection={'border'}
             selectionAnimation={{ duration: 300, borderWidth: 1 }}
-            style={{  top: 0,marginBottom: '10%'  }}
+            style={{  top: 0, }}
             calendarHeaderStyle={{ color: 'black', width: deviceWidth, marginBottom: 10  }}
             calendarColor={'white'}
             highlightColor={'#9265DC'}
@@ -70,15 +70,47 @@ const IndexScreen = ({navigation}) => {
         return (
 
           <TouchableOpacity  style={[styles.card, {borderColor:'white'}]} onPress={()=> navigation.navigate("Show", {id: item.id})}> 
+            <View style={{flexDirection:"row"}}> 
+              <View style={[styles.circle, {backgroundColor: 'purple'}]} />
+              <Text style={[styles.name, {color:item.color}]}> {item.title} </Text>
+              <Text style={styles.timestamp2}> {moment(item.timestamp).fromNow()}  </Text>
 
-            <View style={styles.row}> 
-              <Text style={styles.title}> {item.title} </Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+              {/* <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
                 <Text>
                   Delete
                 </Text>
-              </TouchableOpacity>
+              </TouchableOpacity> */}
               </View>
+
+              <View style={[styles.cardContent]}>
+
+              </View>
+
+
+              <Text style={styles.post}> {item.content}  </Text>
+                  <Image
+                        style={{
+                          borderRadius:5,
+                          marginHorizontal:10,
+                          height: 150,
+                          width: undefined,
+                        }}
+                        source={{uri:'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/177384.jpg'}}
+                        resizeMode="cover"
+                      />
+
+                    <Text style={{marginHorizontal:10, marginTop: 20, fontSize: 1, }}> Meltdown Type </Text> 
+
+                    <View style={[styles.cardContent, styles.tagsContent]}>
+                             {/* {this.renderTags(item)} */}
+                    </View>
+
+                    <View style={{flexDirection: "row", marginTop: 15, marginLeft:15 } }>
+                            <Ionicons name="ios-pin" size={20} color="#C4C6CE"  style={{marginRight: 3}} />
+                            <Text style={styles.timestamp}> Irvine California  </Text>
+                </View>
+
+
             </TouchableOpacity>
 
         )
